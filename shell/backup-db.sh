a3ebbfe63e6fe7d51c551ef4ab1c264aaed1c586
Корнилов А. А <KornilovAA@shuvoe.rg-rus.ru>
2020-07-24 16:46:29 +0300

    ch backup-db

--- shell/backup-db.sh
+++ shell/backup-db.sh
@@ -24,7 +24,6 @@ TAR=$(whereis tar | awk '{print $2}')
 ### Functions block ###
 #######################
 
-# The function for help messages
 function printHelp() {
 echo "
 backup-database, version: $VERSION
@@ -38,6 +37,7 @@ Usage:
 -d|--db-name            - database name for non-file types
 -s|--source             - location of file data for file type
 -o|--output             - destination of backup
+-n|--name               - name of backup archive for file type (default: fsbackup)
 -T|--temp               - temp dir for backup files (default: /tmp)
 --help                  - print this help message
 --debug                 - print result information and exit
@@ -46,122 +46,225 @@ Examples:
 # Make backup of mysql database:
 backup-db -t mysql --db-host localhost --db-name Syslog --db-user rsyslog --db-password P@ssw0rd -o /mnt/storage1 
 # Make backup of files:
-backup-db -t file -s /var/lib/mysql -o /mnt/storage1
+backup-db -t file -s /var/lib/mysql -o /mnt/storage1 -n my-file-backup
 
 
 "
 exit 0
 }
 
-function checkSource() {
-    TARGET=$1
-    if [ ! -d "$TARGET" ]; then
-        echo "Source dir $TARGET does not exist! Aborting..."
-        exit 1
+function printDebug() {
+    printf "%-20s : %-25s\n" "Database host" "$DB_HOST"
+    printf "%-20s : %-25s\n" "Database port" "$DB_PORT"
+    printf "%-20s : %-25s\n" "Database name" "$DB_NAME"
+    printf "%-20s : %-25s\n" "Database user" "$DB_USER"
+    printf "%-20s : %-25s\n" "Database password" "$DB_PASS"
+    printf "%-20s : %-25s\n" "Type of backup" "$TYPE"
+    printf "%-20s : %-25s\n" "Source" "$SRC"
+    printf "%-20s : %-25s\n" "Destination" "$DEST"
+    printf "%-20s : %-25s\n" "Archive name" "$ARC_NAME"
+    printf "%-20s : %-25s\n" "Temp directory" "$TMP"
+    exit 0
+}
+
+function printError() {
+    case "$1" in
+        "a1")
+            echo "Archive name is empty. Use --name to provide it. Using default fsbackup."
+            ;;
+        "a2")
+            echo "Can't find TAR! Please check tar installation. Aborting..."
+            ;;
+        "b1")
+            echo "Backup type is unknown. Use --type to provide it. Use --help for examples. Aborting..."
+            ;;
+        "b3")
+            echo "Syntax error: [-t|--type] mysql|mariadb|file"
+            ;;
+        "b4")
+            echo "Syntax error! Please, use '--help' to show usage examples."
+            ;;
+        "m1")
+            echo "Can't find MYSQL! Please check mysql/mariadb pkg installation. Aborting..."
+            ;;
+        "m2")
+            echo "Can't find MYSQLSHOW! Please check mysql/mariadb pkg installation. Aborting..."
+            ;;
+        "m3")
+            echo "Can't find MYSQLDUMP! Please check mysql/mariadb pkg installation. Aborting..."
+            ;;
+        "m4")
+            echo "Can't connect to server "$DB_HOST" ! Please, check connection details. Aborting..."
+            ;;
+        "m5")
+            echo "Can't connect to server "$DB_HOST" ! WARNING: You have tried to connect to local server! Aborting..."
+            ;;
+        "m6")
+            echo "Access denied for "$DB_USER" ! Please, check connection details. Aborting..."
+            ;;
+        "m7")
+            echo "Database "$DB_NAME" does not exist! Please, check connection details. Aborting..."
+            ;;
+        "t1")
+            echo "TMP dir "$TMP" does not exist! Trying to create..."
+            ;;
+        "t2")
+            echo "Can't create or read TMP dir! Check folder "$TMP" permissons. Aborting..."
+            ;;
+        "t3")
+            echo "TMP dir is not writable. Check folder "$TMP" permissons. Aborting...";
+            ;;
+        "t4")
+            echo "Temp dir is empty. Use --temp to provide it. Using default /tmp."
+            ;;
+        "s1")
+            echo "Source dir "$SRC" does not exist! Aborting..."
+            ;;
+        "s2")
+            echo "Source dir is not readable. Check folder "$SRC" permissons. Aborting..."
+            ;;
+        "s3")
+            echo "Source dir is empty. Use --source to provide it. Aborting..."
+            ;;
+        "d1")
+            echo "Destination dir "$DST" does not exist! Trying to create..."
+            ;;
+        "d2")
+            echo "Can't create or read destination dir. Check folder "$DST" permissons. Aborting..."
+            ;;
+        "d3")
+            echo "Destination dir is not writable. Check folder "$DST" permissons. Aborting..."
+            ;;
+        "d4")
+            echo "Destination dir is empty. Use --output to provide it. Aborting..."
+            ;;
+        "c1")
+            echo "Database user is empty. Use --db-user to provide it. Aborting..."
+            ;;
+        "c2")
+            echo "Database password is empty. It is not secure! Use --db-password to provide it."
+            ;;
+        "c3")
+            echo "Database host is empty. Use --db-host to provide it. Aborting..."
+            ;;
+        "c4")
+            echo "Database name is empty. Use --db-name to provide it. Aborting..."
+            ;;
+        "c5")
+            echo "Database port is empty. Use --db-port to provide it. Using 3306 as default."
+            ;;
+        "w1")
+            echo "[WARN] Creating dump problem..."
+            ;;
+        "w2")
+            echo "[WARN] Creating archive problem..."
+            ;;
+        "w3")
+            echo "[WARN] Coping file problem..."
+            ;;
+        *)
+            echo "Unknown error."
+}
+
+function checkTmp() {
+    if [ -z "$TMP" ]; then TMP="/tmp"; fi
+    if [ ! -d "$TMP" ]; then
+        printError t1
+        mkdir -p $TMP
+        if [ ! $? -eq 0 ]; then printError t2; exit 1; fi
     fi
+    if [ ! -w "$TMP" ]; then printError t3; exit 1; fi
+}
+
+function checkSource() {
+    if [ ! -d "$SRC" ]; then printError s1; exit 1; fi
+    if [ ! -r "$SRC" ]; then printError s2; exit 1; fi
 }
 
 function checkDestination() {
-    TARGET=$1
-    if [ ! -d "$TARGET" ]; then
-        echo "Destination dir $TARGET does not exist! Trying to create..."
-        mkdir -p $TARGET
-        if [ $? -eq 1 ]; then
-            echo "Can't create or read dir $TARGET! Aborting..."
-            exit 1
-        fi
+    if [ ! -d "$DST" ]; then
+        printError d1
+        mkdir -p $DST
+        if [ ! $? -eq 0 ]; then printError d2; exit 1; fi
     fi
+    if [ ! -w "$DST" ]; then printError d3; exit 1; fi
 }
 
 function checkMysql {
-    if [ $# -lt 5 ]; then
-        echo "Syntax error! Please, use this function like this one: checkMysql <db name> <db host> <db port> <db user> <db password>"
-        exit 1
-    fi
-    if [ -z "$MYSQL" ]; then
-        echo "Can't find MYSQL! Please check mysql/mariadb pkg installation. Aborting..."
-        exit 1
-    fi
-    if [ -z "$MYSQLSHOW" ]; then
-        echo "Can't find MYSQLSHOW! Please check mysql/mariadb pkg installation. Aborting..."
-        exit 1
-    fi
-    if [ -z "$MYSQLDUMP" ]; then
-        echo "Can't find MYSQLDUMP! Please check mysql/mariadb pkg installation. Aborting..."
-        exit 1
-    fi
-    TARGETNAME=$1
-    TARGETHOST=$2
-    TARGETPORT=$3
-    TARGETUSER=$4
-    TARGETPWD=$5
-
-# debug string
-#    echo "DB name:$TARGETNAME DB host:$TARGETHOST DB port:$TARGETPORT DB user:$TARGETUSER DB password:$TARGETPWD"
-    echo "Checking database $TARGETNAME ..."
-    DBCHECK=$("${MYSQLSHOW}" -u"${TARGETUSER}" -p"${TARGETPWD}" -h "${TARGETHOST}" -P "${TARGETPORT}" "${TARGETNAME}" 2>&1)
-    if [[ $DBCHECK == *"Database: ${TARGETNAME}"* ]]; then
+    if [ -z "$MYSQL" ]; then printError m1; exit 1; fi
+    if [ -z "$MYSQLSHOW" ]; then echo printError m2; exit 1; fi
+    if [ -z "$MYSQLDUMP" ]; then echo printError m3; exit 1; fi
+    echo "Checking database $DB_NAME ..."
+    DBCHECK=$("${MYSQLSHOW}" -u"${DB_USER}" -p"${DB_PASS}" -h "${DB_HOST}" -P "${DB_PORT}" "${DB_NAME}" 2>&1)
+    if [[ $DBCHECK == *"Database: ${DB_NAME}"* ]]; then
         echo "Checked!"
     else
-        if [[ $DBCHECK == *"Unknown MySQL server host"* ]]; then
-            echo "Can't connect to server $TARGETHOST !\nPlease, check connection details. Aborting..."
-            exit 1
-        fi
-        if [[ $DBCHECK == *"connect to local MySQL server"* ]]; then
-            echo "Can't connect to server $TARGETHOST !\nWARNING: You have tried to connect to local server! Aborting..."
-            exit 1
-        fi
-        if [[ $DBCHECK == *"Access denied for user"* ]]; then
-            echo "Access denied for $TARGETUSER !\nPlease, check connection details. Aborting..."
-            exit 1
-        fi
-        if [[ $DBCHECK == *"Unknown database"* ]]; then
-            echo "Database $TARGETNAME does not exist!\nPlease, check connection details. Aborting..."
-            exit 1
-        fi
+        if [[ $DBCHECK == *"Unknown MySQL server host"* ]]; then printError m4; exit 1; fi
+        if [[ $DBCHECK == *"connect to local MySQL server"* ]]; then printError m5; exit 1; fi
+        if [[ $DBCHECK == *"Access denied for user"* ]]; then printError m6; exit 1; fi
+        if [[ $DBCHECK == *"Unknown database"* ]]; then printError m7; exit 1; fi
     fi
 }
 
 function checkParam() {
     case "$TYPE" in
         "mysql"|"mariadb")
-            if [ -z "$DB_USER" ]; then echo "Database user is empty. Use --db-user to provide it. Aborting..."; exit 1; fi
-            if [ -z "$DB_PASS" ]; then echo "Database password is empty. It is not secure! Use --db-password to provide it."; fi
-            if [ -z "$DB_HOST" ]; then echo "Database host is empty. Use --db-host to provide it. Aborting..."; exit 1; fi
-            if [ -z "$DB_NAME" ]; then echo "Database name is empty. Use --db-name to provide it. Aborting..."; exit 1; fi
-            if [ -z "$DB_PORT" ]; then echo "Database port is empty. Use --db-port to provide it. Using 3306 as default."; fi
-            if [ -z "$DST" ]; then echo "Destination dir is empty. Use --output to provide it. Aborting..."; exit 1; fi
-            if [ -z "$TMP" ]; then echo "Temp dir is empty. Use --temp to provide it. Using default /tmp."; fi
+            if [ -z "$DB_USER" ]; then printError c1; exit 1; fi
+            if [ -z "$DB_PASS" ]; then printError c2; fi
+            if [ -z "$DB_HOST" ]; then printError c3; exit 1; fi
+            if [ -z "$DB_NAME" ]; then printError c4; exit 1; fi
+            if [ -z "$DB_PORT" ]; then printError c5; fi
+            if [ -z "$DST" ]; then printError d4; exit 1; fi
+            if [ -z "$TMP" ]; then printError t4; fi
             ;;
         "file")
-            if [ -z "$SRC" ]; then echo "Source dir is empty. Use --source to provide it. Aborting..."; exit 1; fi
-            if [ -z "$DST" ]; then echo "Destination dir is empty. Use --output to provide it. Aborting..."; exit 1; fi
-            if [ -z "$TMP" ]; then echo "Temp dir is empty. Use --temp to provide it. Using default /tmp."; fi
+            if [ -z "$SRC" ]; then printError s3; exit 1; fi
+            if [ -z "$DST" ]; then printError d4; exit 1; fi
+            if [ -z "$TMP" ]; then printError t4; fi
+            if [ -z "$ARC_NAME" ]; then printError a1; fi
             ;;
         *)
-            echo "Backup type is unknown. Use --type to provide it.\nUse --help for examples. Aborting..."
+            printError b1
             exit 1
             ;;
     esac
 }
 
 function backupMysql() {
-    if [ -z "$TAR" ]; then
-        echo "Can't find TAR! Please check tar installation. Aborting..."
-        exit 1
-    fi
-    if [ -z "$TMP" ]; then
-        TMP="/tmp"
-    fi
+    if [ -z "$TAR" ]; then printError a2; exit 1; fi
+    if [ -z "$TMP" ]; then TMP="/tmp"; fi
 # Create dump in tmp folder
     $MYSQLDUMP --column-statistics=0 -u"$DB_USER" -p"$DB_PASS" -h "$DB_HOST" -P "$DB_PORT" "$DB_NAME" > "$TMP"/db-"$DB_NAME"-"$TIMESTAMP".sql
-    if [ ! $? -eq 0 ]; then echo "[WARN] Creating dump problem..."; fi
-# Archiving data
+    if [ ! $? -eq 0 ]; then printError w1; fi
+# Coping and archiving data
     $TAR -czf "$TMP"/db-"$DB_NAME"-"$TIMESTAMP".tar.gz -C "$TMP" db-"$DB_NAME"-"$TIMESTAMP".sql
-    if [ ! $? -eq 0 ]; then echo "[WARN] Creating archive problem..."; fi
-# Copy archive to destination folder
+    if [ ! $? -eq 0 ]; then printError w2; fi
     cp "$TMP"/db-"$DB_NAME"-"$TIMESTAMP".tar.gz "$DST"/db-"$DB_NAME"-"$TIMESTAMP".tar.gz
-    if [ ! $? -eq 0 ]; then echo "[WARN] Coping file problem..."; fi
+    if [ ! $? -eq 0 ]; then printError w3; fi
+}
+
+function backupFile() {
+    if [ -z "$TAR" ]; then printError a2; exit 1; fi
+    if [ -z "$TMP" ]; then TMP="/tmp"; fi
+    if [ -z "$ARC_NAME" ]; then ARC_NAME="fsbackup"; fi
+# Coping and archiving data
+    $TAR -czf "$TMP"/"$ARC_NAME"-"$TIMESTAMP".tar.gz -C "$SRC" .
+    if [ ! $? -eq 0 ]; then printError w2; fi
+    cp "$TMP"/"$ARC_NAME"-"$TIMESTAMP".tar.gz "$DST"/"$ARC_NAME"-"$TIMESTAMP".tar.gz
+    if [ ! $? -eq 0 ]; then printError w3; fi
+}
+
+function cleanTmp() {
+    case "$TYPE" in
+        "mysql"|"mariadb")
+            rm -rf "$TMP"/db-"$DB_NAME"-"$TIMESTAMP".tar.gz
+            ;;
+        "file")
+            rm -rf "$TMP"/"$ARC_NAME"-"$TIMESTAMP".tar.gz
+            ;;
+        *)
+            ;;
+    esac
 }
 
 
@@ -171,7 +274,6 @@ function backupMysql() {
 
 # Parsing given arguments
 if [[ $# -eq 0 ]]; then printHelp; fi
-
 while [[ $# -gt 0 ]]
 do
     ARG="$1"
@@ -186,7 +288,7 @@ do
                     TYPE="file"
                     ;;
                 *)
-                    echo "Syntax error: [-t|--type] mysql|mariadb|file"
+                    printError b3
                     exit 1
                     ;;
             esac
@@ -220,79 +322,57 @@ do
             DST=$2
             shift 2
             ;;
+        "-n"|"--name")
+            ARC_NAME=$2
+            shift 2
+            ;;
         "-T"|"--temp")
             TMP=$2
             shift 2
             ;;
         "--debug")
-            DEBUG="YES"
-            shift
+            printDebug
             ;;
         "--help")
             printHelp
             ;;
         *)
-            echo "Syntax error! Please, use '--help' to show usage examples."
+            printError b4
             exit 1
             ;;
     esac
 done
+# Check if no params set
+if [ $# -eq 0 ]; then printHelp; fi
+# Check input params
+checkParam
+# Set default params
+if [ -z "TMP" ]; then TMP="/tmp"; fi
+case "$TYPE" in
+    "mysql"|"mariadb")
+        if [ -z "$DB_PORT" ]; then DB_PORT="3306"; fi
+        ;;
+    "file")
+        if [ -z "$ARC_NAME" ]; then ARC_NAME="fsbackup"; fi
+        ;;
+    *)
+        ;;
+esac
+# Check tmp folder
+checkTmp
+# Check destination
+checkDestination
+# Backuping
+case "$TYPE" in
+    "mysql"|"mariadb")
+        if [ -z "$DB_PORT" ]; then DB_PORT="3306"; fi
+        ;;
+    "file")
+        if [ -z "$ARC_NAME" ]; then ARC_NAME="fsbackup"; fi
+        ;;
+    *)
+        ;;
+esac
 
-# Set defaults if arguments aren't presented
-if ! [[ "$TMP" ]]; then TMP="/tmp"; fi
-if ! [[ "$DB_NAME" ]]; then DB_NAME="zabbix"; fi
-if ! [[ "$DB_HOST" ]]; then DB_HOST="localhost"; fi
-if ! [[ "$DEST" ]]; then DEST=$(pwd); LOGFILE="$DEST/zbx_backup.log"; fi
-if ! [[ "$ROTATION" ]]; then ROTATION=10; fi
-if ! [[ "$ZBX_CATALOGS" ]]; then ZBX_CATALOGS=("/usr/lib/zabbix" "/etc/zabbix"); fi
-if [[ "$USE_COMPRESSION" ]] && ! [[ $(command -v "$COMPRESS_WITH") ]]; then echo "ERROR: '$COMPRESS_WITH' utility not found."; exit 1; fi
-
-#
-# A lot of checks, trying to make this script more friendly
-#
-
-# Check '-b' option is provided
-if ! [[ "$B_UTIL"  ]]; then echo "ERROR: You must provide backup utility ('-b')."; exit 1; fi
-
-# Checking TMP and DST directories existing
-if ! [[ -d "$TMP" ]]; then if ! mkdir -p $TMP; then echo "ERROR: Cannot create temp directory ($TMP)."; exit 1; fi; fi
-if ! [[ -d "$DEST" ]]; then echo "ERROR: $TIMESTAMP : Destination directory doesn't exists." | tee -a "./zbx_backup.log"; exit 1; fi
-
-# Enter the password if it equal to '-'
-if  [[ "$DB_PASS" == "-" ]]
-then
-        read -s -p "Please, enter the password for user '$DB_USER': " DB_PASS
-        echo -e "\n"
-fi
-
-# Check if username is provided
-if [[ "$DB_USER" ]]
-then
-        if [[ "$DB_USER" =~ ^[0-9]|- ]] && ! [[ "$FORCE" ]]
-        then
-                echo "WARNING: Username '$DB_USER' looks wrong (starts with '-' or digit). Use '--force' if it's OK."
-                exit 1
-        fi
-else
-        echo "ERROR: You must provide username to connect to the database ('-u|--db-user')."
-        exit 1
-fi
-
-
-if [[ "$DEBUG" == "YES" ]]
-then
-        function join { local IFS="$1"; shift; echo "$*"; }
-
-        printf "%-20s : %-25s\n" "Database host" "$DB_HOST"
-        printf "%-20s : %-25s\n" "Database name" "$DB_NAME"
-        printf "%-20s : %-25s\n" "Database user" "$DB_USER"
-        printf "%-20s : %-25s\n" "Database password" "$DB_PASS"
-        printf "%-20s : %-25s\n" "Type of backup" "$TYPE"
-        printf "%-20s : %-25s\n" "Source" "$SRC"
-        printf "%-20s : %-25s\n" "Destination" "$DEST"
-        printf "%-20s : %-25s\n" "Logfile location" "$LOGFILE"
-        printf "%-20s : %-25s\n" "Temp directory" "$TMP"
-        exit 0
-fi
 
 exit 0
