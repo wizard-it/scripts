25d4cfddd3d2e922a79d5a93776e9169414acf3c
Корнилов А. А <KornilovAA@shuvoe.rg-rus.ru>
2020-08-18 16:37:12 +0300

    changes

---
+++ bat/install_zabbix_agent.bat
@@ -0,0 +1,19 @@
+@ECHO ON
+IF NOT EXIST "%ProgramFiles%\Zabbix Agent" (
+ECHO "Make DIR"
+MD "%ProgramFiles%\Zabbix Agent"
+
+xcopy \\fsc01\softdistrib$\zabbix_agent\latest_x64 "%ProgramFiles%\Zabbix Agent" /E /Y /Q
+ECHO "Copying AGENT FILES completed"
+
+CD "%ProgramFiles%\Zabbix Agent"
+zabbix_agentd.exe --config zabbix_agentd.conf --install
+ECHO "Zabbix Agent Installed"
+
+zabbix_agentd.exe --start
+ECHO "Zabbix Agent Running"
+
+) ELSE (
+
+ECHO "DIR is available!"
+)
