857615ca3386eca69d37e39c58b12a8f20628b1e
Корнилов А. А <KornilovAA@shuvoe.rg-rus.ru>
2020-11-19 14:45:46 +0300

    changes

---
+++ bat/uninstall_chrome.bat
@@ -0,0 +1,4 @@
+@ECHO ON
+FOR /F "skip=2 tokens=2,*" %%A IN ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\Google Chrome" /v Version') DO set "CHROMEVER=%%B"
+
+"C:\Program Files\Google\Chrome\Application\%CHROMEVER%\Installer\setup.exe" -uninstall -multi-install -chrome -system-level -force-uninstall
