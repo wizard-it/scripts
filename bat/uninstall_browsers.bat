d4b93908886a43b33c790edaa819374c6333f22e
Корнилов А. А <KornilovAA@shuvoe.rg-rus.ru>
2020-12-09 11:18:13 +0300

    changes

--- bat/uninstall_browsers.bat
+++ bat/uninstall_browsers.bat
@@ -24,3 +24,21 @@ FOR /f "delims=" %%c in ('dir/b/ad-h "C:\Users"') do (
         )
     )
 )
+
+@ECHO OFF
+if exist "%ProgramFiles%\Mozilla Firefox\uninstall\helper.exe" ("%ProgramFiles%\Mozilla Firefox\uninstall\helper.exe" /S)
+if exist "%ProgramFiles(x86)%\Mozilla Firefox\uninstall\helper.exe" ("%ProgramFiles(x86)%\Mozilla Firefox\uninstall\helper.exe" /S)
+for /f "delims=" %%a in ('dir/b/ad-h "C:\Users"') do (
+    if exist "C:\Users\%%a\AppData\Local\Mozilla Firefox\uninstall\helper.exe" (
+        "C:\Users\%%a\AppData\Local\Mozilla Firefox\uninstall\helper.exe" /S
+    )
+)
+
+@ECHO OFF
+if exist “%ProgramFiles%\Opera\launcher.exe” (“%ProgramFiles%\Opera\launcher.exe” /uninstall /silent)
+if exist “%ProgramFiles(x86)%\Opera\launcher.exe” (“%ProgramFiles(x86)%\Opera\launcher.exe” /uninstall /silent)
+for /f "delims=" %%b in ('dir/b/ad-h "C:\Users"') do (
+    if exist "C:\Users\%%b\AppData\Local\Programs\Opera\launcher.exe" (
+        "C:\Users\%%b\AppData\Local\Programs\Opera\launcher.exe" /uninstall /silent
+    )
+)
