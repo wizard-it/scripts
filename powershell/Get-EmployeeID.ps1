4546142d049a584e869e1ed888705e8fb2a0b1db
Kornilov A.A <spellbook33@gmail.com>
2021-04-06 15:51:02 +0300

    changes

---
+++ powershell/Get-EmployeeID.ps1
@@ -0,0 +1,58 @@
+﻿Function Get-EmployeeID {
+<#
+        .SYNOPSIS
+        Поиск EmployeeID из выгрузки 1С
+
+        .DESCRIPTION
+        Поиск EmployeeID из выгрузки 1С.
+
+        .EXAMPLE
+        C:\PS>Get-EmployeeID -SurName Ив
+        employeeid sn      givenName middleName    title                        division                  department                                   
+        ---------- --      --------- ----------    -----                        --------                  ----------                                   
+        0000001134 Иванова Валентина Николаевна    Укладчик-упаковщик           Производственная дирекция Цех фасовки и упаковки
+                
+        Описание 
+        -----------
+
+        EmployeeID пользователей Ив*                       
+
+        .EXAMPLE
+        C:\PS>Get-EmployeeID -SurName Ив -CSVPath "C:\tmp\employees.csv"
+        employeeid sn      givenName middleName    title                        division                  department                                   
+        ---------- --      --------- ----------    -----                        --------                  ----------                                   
+        0000001134 Иванова Валентина Николаевна    Укладчик-упаковщик           Производственная дирекция Цех фасовки и упаковки                       
+        
+        Описание 
+        -----------
+
+        Изменить пусть к файлу CSV
+
+        .NOTES
+        Organization: JSC "Gedeon Richter-RUS"
+        Authors:  Piskunov Dmitry
+
+    #>
+    [CmdLetBinding()]
+     Param (
+     [ValidatePattern('^[А-ЯЁ][а-яё]+\s?$')][string]$SurName,
+     [string]$CSVPath = "\\fsc01\logs$\csv2ldap\employees.csv"
+     )
+    
+    #Импорт CSV (делаем так, потому что Import-Csv не смог)
+    $getcsv = Get-Content -Path $CSVPath
+    $csv = ConvertFrom-Csv $getcsv -Delimiter ";"
+
+    #Время формирования выгрузки
+    $CSVCreationtime = (Get-Item $CSVPath).LastWriteTime
+    $CSVCreationtimeConv = $CSVCreationtime.ToString("dd.MM.yyyy HH:mm") 
+
+    #Поиск
+    $out = $csv | Where-Object {$_.sn -like "*$SurName*"} | select employeeid,sn,givenName,middleName,title | ft | Out-String
+    
+    #Вывод
+    Write-Host $out
+    Write-Host "Дата изменения выгрузки: $CSVCreationtimeConv"
+    Clear-Variable -Name CSVPath, getcsv, csv, CSVCreationtime, CSVCreationtimeConv, out
+
+}
