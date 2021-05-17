cb4e511bb37a3f18b1f230770456dd1d6ae4cc63
Kornilov A.A <spellbook33@gmail.com>
2021-05-17 15:40:37 +0300

    changes

--- powershell/Create-ServerReportLinux.ps1
+++ powershell/Create-ServerReportLinux.ps1
@@ -124,26 +124,21 @@
             }
 
             #region Определение роли сервера
-            switch -wildcard ($Server.Description) {
-                "*Mail*" {$Role = "Сервер приложений MS Exchange."}
-                "*Domain*" {$Role = "Контроллер домена."}
-                "*DB*" {$Role = "Сервер баз данных."}
-                "*Monitor*" {$Role = "Сервер приложений Мониторинг."}
-                "*APP*" {$Role = "Сервер приложений."}
-                "*EMS*" {$Role = "Сервер мониторинга EMS."}
-                "*RUDIS*" {$Role = "Сервер приложений RUDIS."}
-                "Experium*" {$Role = "Сервер приложений Experium."}
-                "*Netbox*" {$Role = "Сервер приложений Netbox."}
-                "*Print*" {$Role = "Сервер печати."}
-                "*SAP*" {$Role = "Сервер приложений SAP."}
-                "*File*" {$Role = "Файловый сервер"}
-                "*Hyper-V*" {$Role = "Хост виртуализации Hyper-V."}
-                "*Terminal*" {$Role = "Терминальный сервер."}
-                "*Web*" {$Role = "Сервер Web приложений."}
-                "*Kubernet*" {$Role = "Узел Kubernets."}
-                "*Load*" {$Role = "Балансировщик нагрузки."}
-                default {$Role = "Роль сервера не определена!"}
-            }
+#            switch -wildcard ($Server.Description) {
+#                "*Mail*" {$Role = "Сервер приложений MS Exchange."}
+#                "DB" {$Role = "Сервер баз данных."}
+#                "APP" {$Role = "Сервер приложений."}
+#                "EMS" {$Role = "Сервер мониторинга EMS."}
+#                "RUDIS" {$Role = "Сервер приложений RUDIS."}
+#                "Print*" {$Role = "Сервер печати."}
+#                "SAP*" {$Role = "Сервер приложений SAP."}
+#                "File*" {$Role = "Файловый сервер"}
+#                "Hyper-V" {$Role = "Хост виртуализации Hyper-V."}
+#                "Terminal*" {$Role = "Терминальный сервер."}
+#                "Web*" {$Role = "Сервер Web приложений."}
+#                default {$Role = "Роль сервера не определена!"}
+#            }
+            $Role = "$($Server.Description)"
             $oXmlRoot.SetAttribute("Role","$($Role)")
             #endregion
             
