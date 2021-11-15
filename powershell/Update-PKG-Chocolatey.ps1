60d893d5a10df6b758b3f13883f23c52a07e030e
Kornilov A.A <spellbook33@gmail.com>
2021-11-15 16:58:27 +0300

    changes

--- powershell/Update-PKG-Chocolatey.ps1
+++ powershell/Update-PKG-Chocolatey.ps1
@@ -22,73 +22,97 @@ function Update-Pkg-Choco() {
         [Parameter (Mandatory=$true)]
         [string]$key,
         [switch]$save,
-        [string]$builddir
+        [string]$builddir = [System.IO.Path]::GetTempPath()
     )
 
-    if (!$builddir) { $builddir = [System.IO.Path]::GetTempPath() }
-    $prefix = "-rg"
-    $defaultRepo = "https://community.chocolatey.org/api/v2/"
-
-    if ( -not (choco -v) ) { 
-        Write-Host "Chocolatey exe problem, check application is installed. Aborting..."
-        return 1
-    }
-
-    foreach ($pkgName in $pkglist) {
-        
+    function rebuildPkg {
+        param (
+            [string]$pkgName,
+            [string]$localRepo,
+            [string]$localKey,
+            [string]$workDir
+        )
+        cd $workDir
+        $defaultRepo = "https://community.chocolatey.org/api/v2/"
         $origPkgInfo = choco search $pkgName -e -s="$defaultRepo"
         $origPkgVer = $($($origPkgInfo | Select-String -Pattern $pkgName) -split ' ')[1]
         [int]$origPkgNumber = $origPkgVer -replace '\.',''
-        $localPkgInfo = choco search $pkgName -e -s="$repo"
+        $localPkgInfo = choco search $pkgName -e -s="$localRepo"
         if ($localPkgInfo | Select-String -Pattern $pkgName) {
             $localPkgVer = $($($localPkgInfo | Select-String -Pattern $pkgName) -split ' ')[1]
         } else {
             $localPkgVer = "0."
         }
         [int]$localPkgNumber = $localPkgVer -replace '\.',''
-
         if ( ($origPkgNumber -gt $localPkgNumber) -or ($origPkgNumber -eq $localPkgNumber)) {
-            Write-Host "Downloading new version of $pkgName package to $builddir ..."
-#            [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-            Invoke-WebRequest -Uri $defaultRepo/package/$pkgName/$origPkgVer -OutFile $builddir\\$pkgName.$origPkgVer.zip
-            cd $builddir
-            Expand-Archive .\$pkgName.$origPkgVer.zip -Force
-            cd "$pkgName.$origPkgVer"
-            rmdir .\_rels -Force -Recurse
-            rmdir .\package -Force -Recurse
-            del *.xml
-            $origScript = $origScript = Get-Content -Path .\tools\chocolateyInstall.ps1
-            $origScript | Select-String -Pattern '^\$url' | Set-Content -Path .\$pkgName.vars.ps1
-            . .\$pkgName.vars.ps1
+            Write-Host "Downloading new version of $pkgName package to $workDir ..."
+            [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+            Invoke-WebRequest -Uri $defaultRepo/package/$pkgName/$origPkgVer -OutFile $workDir\$pkgName.zip
+            Expand-Archive $workDir\$pkgName.zip -Force
+            rmdir $workDir\$pkgName\_rels -Force -Recurse
+            rmdir $workDir\$pkgName\package -Force -Recurse
+            del $workDir\$pkgName\*.xml
             [int]$count = ($origPkgVer -split '\.').Count
             [int]$pos = $count - 2
             [int]$lastVer = ($origPkgVer -split '\.')[-1]
             $newPkgVer = (($origPkgVer -split '\.')[0..$pos] -join '.') + "." + ($lastVer + 1)
+            $origSpec = Get-Content -Path $workDir\$pkgName\$pkgName.nuspec
+            $origSpec -replace "<version>$origPkgVer","<version>$newPkgVer" | Set-Content -Path $workDir\$pkgName\$pkgName.nuspec -Force
+            $matchDep = Select-String -Path $workDir\$pkgName\$pkgName.nuspec -Pattern "dependency id"
+            if ($matchDep) {
+                foreach ($str in $matchDep) {
+                    $pool = $str -split '\s+'
+                    $depName = $pool[2] -replace 'id=|\"',''
+                    $origDepVer = $pool[3] -replace 'version=|\"|\[|\]',''
+                    $origSpec = Get-Content -Path $workDir\$pkgName\$pkgName.nuspec
+                    $newSpec = "$workDir\$pkgName\$pkgName.nuspec.new"
+                    [int]$count = ($origDepVer -split '\.').Count
+                    [int]$pos = $count - 2
+                    [int]$lastOrigDepVer = ($origDepVer -split '\.')[-1]
+                    $newDepVer = (($origDepVer -split '\.')[0..$pos] -join '.') + "." + ($lastOrigDepVer + 1)
+                    foreach ($specstr in $origSpec) {
+                        if ($specstr -match "<dependency id=`"$depName") {
+                            $newspecstr = $specstr -replace $origDepVer, $newDepVer
+                            Add-content -Path $newSpec -value $newspecstr
+                        } else {
+                            Add-content -Path $newSpec -value $specstr
+                        }
+                    }
+                    Move-Item -Path $workDir\$pkgName\$pkgName.nuspec -Destination $workDir\$pkgName\$pkgName.nuspec.old
+                    Copy-Item -Path $workDir\$pkgName\$pkgName.nuspec.new -Destination $workDir\$pkgName\$pkgName.nuspec
+                    rebuildPkg -pkgName $depName -origPkgVer $origDepVer -workDir $workDir
+                }
+            }
+            $origScript = $origScript = Get-Content -Path $workDir\$pkgName\tools\chocolateyInstall.ps1
+            $origScript | Select-String -Pattern '^\$url' | Set-Content -Path $workDir\$pkgName\$pkgName.vars.ps1
+            . $workDir\$pkgName\$pkgName.vars.ps1
             if ($url) { $file = $($url -split '/')[-1] }
             if ($url32) { $file = $($url32 -split '/')[-1] }
-            if ($url64) { $file64 = $($url64 -split '/')[-1] }
-            mkdir .\tools\64 | Out-Null
+            if ($url64) { $file64 = $($url64 -split '/')[-1] }        
+            mkdir $workDir\$pkgName\tools\64 | Out-Null
             Write-Host "Downloading source files..."
-            if ($url) { Invoke-WebRequest -Uri $url -OutFile tools\\$file }
-            if ($url32) { Invoke-WebRequest -Uri $url32 -OutFile tools\\$file }
-            if ($url64) { Invoke-WebRequest -Uri $url64 -OutFile tools\\64\\$file64 }
-            $spec = Get-Content -Path .\$pkgName.nuspec
-            $spec -replace "$origPkgVer","$newPkgVer" | Set-Content -Path .\$pkgName.nuspec -Force
-            $origScript -replace '^\$ErrorAction.*',"`$toolsDir = `"`$`(Split-Path -parent `$MyInvocation.MyCommand.Definition`)`"`r`n`$ErrorActionPreference = `'Stop`'" -replace '^\$url =.*',"`$url = `"`$toolsDir\$file`"" -replace '^\$url32.*',"`$url32 = `"`$toolsDir\$file`"" -replace '^\$url64.*', "`$url64 = `"`$toolsDir\64\$file64`"" | Set-Content -Path .\tools\chocolateyInstall.ps1 -Force
-            choco pack .\$pkgName.nuspec
-            if ($save) {
-                Write-Host "New pkg $pkgName has been saved to $builddir\$pkgName.$newPkgVer.nupkg"
-                Copy-Item -Path ".\$pkgName.$newPkgVer.nupkg" -Destination "$builddir\"
-            }
-            choco push -s="$repo" -k="$key" ".\$pkgName.$newPkgVer.nupkg" --force
+            if ($url) { Invoke-WebRequest -Uri $url -OutFile $workDir\$pkgName\tools\$file }
+            if ($url32) { Invoke-WebRequest -Uri $url32 -OutFile $workDir\$pkgName\tools\$file }
+            if ($url64) { Invoke-WebRequest -Uri $url64 -OutFile $workDir\$pkgName\tools\64\$file64 }
+            $origScript -replace '^\$ErrorAction.*',"`$toolsDir = `"`$`(Split-Path -parent `$MyInvocation.MyCommand.Definition`)`"`r`n`$ErrorActionPreference = `'Stop`'" -replace '^\$url =.*',"`$url = `"`$toolsDir\$file`"" -replace '^\$url32.*',"`$url32 = `"`$toolsDir\$file`"" -replace '^\$url64.*', "`$url64 = `"`$toolsDir\64\$file64`"" | Set-Content -Path $workDir\$pkgName\tools\chocolateyInstall.ps1 -Force
+            Write-Host "Building pkg $pkgName version $newPkgVer, old version is $origPkgVer in dir $workDir\$pkgName"
+            choco pack $workDir\$pkgName\$pkgName.nuspec       
+            choco push -s="$localRepo" -k="$localKey" "$workDir\$pkgName.$newPkgVer.nupkg" --force
             Write-Host "Cleaning build dir..."
-            cd $builddir
-            rmdir ".\$pkgName.$origPkgVer" -Recurse -Force
-            del ".\$pkgName.$origPkgVer.zip"
-
+            rmdir "$workDir\$pkgName" -Recurse -Force
+            del "$workDir\$pkgName.zip"
+            if (!$save) { del $workDir\$pkgName.$newPkgVer.nupkg }
         } else {
             Write-Host "Package $pkgName is up to date"
         }
     }
 
+    if ( -not (choco -v) ) { 
+        Write-Host "Chocolatey exe problem, check application is installed. Aborting..."
+        return 1
+    }
+
+    foreach ($name in $pkglist) {
+        rebuildPkg -pkgName $name -localRepo $repo -localKey $key -workDir $builddir
+    }
 }
