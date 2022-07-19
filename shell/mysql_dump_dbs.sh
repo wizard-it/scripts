5bf4833b5b29aebe420146fc6a3aa42cb62013e9
Kornilov A.A <spellbook33@gmail.com>
2022-07-19 16:06:26 +0300

    changes

---
+++ shell/mysql_dump_dbs.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Destiny folder where backups are stored
+DEST=/opt/backup/mysql
+
+CURRDATE=$(date +"%F")
+
+# Hostname where MySQL is running
+HOSTNAME="localhost"
+# User name to make backup
+USER="backup"
+# File where has the mysql user password
+PASS="$(cat /usr/local/etc/sqlpass)"
+
+DATABASES=$(mysql -h $HOSTNAME -u $USER -p$PASS -e "SHOW DATABASES;" | tr -d "| " | grep -v Database)
+
+[ ! -d $DEST ] && mkdir -p $DEST
+
+for db in $DATABASES; do
+  FILE="${DEST}/$db.sql.gz"
+  FILEDATE=
+
+  # Be sure to make one backup per day
+  [ -f $FILE ] && FILEDATE=$(date -r $FILE +"%F")
+  [ "$FILEDATE" == "$CURRDATE" ] && continue
+
+  [ -f $FILE ] && mv "$FILE" "${FILE}.old"
+  mysqldump --single-transaction --routines --quick -h $HOSTNAME -u $USER -p$PASS -B $db | gzip > "$FILE"
+#  chown bacula:disk "$FILE"
+  rm -f "${FILE}.old"
+done
