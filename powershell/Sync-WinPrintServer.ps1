771add64e16f48a5cf07c594b2fac876af7795a9
Kornilov A.A <spellbook33@gmail.com>
2022-10-21 14:15:44 +0300

    changes

---
+++ powershell/Sync-WinPrintServer.ps1
@@ -0,0 +1,16 @@
+$ProgramPath = 'C:\Windows\System32\Spool\Tools\PrintBrm.exe'
+$ArchiveName1 = 'print-backup-log'
+$ArchiveName2 = 'print-restore-log'
+
+$SourceServer = 'shv-vprn01'
+$DestServer = 'shv-vprn02'
+
+$ConfigFilePath = 'C:\Scripts\prn-config.printerExport'
+
+$Arguments = "-B -S $SourceServer -F $ConfigFilePath"
+Start-process $ProgramPath -ArgumentList $Arguments -Wait -RedirectStandardOutput ".\$ArchiveName1.txt"
+
+$Arguments = "-R -S $DestServer -F $ConfigFilePath"
+Start-process $ProgramPath -ArgumentList $Arguments -Wait -RedirectStandardOutput ".\$ArchiveName2.txt"
+
+Del $ConfigFilePath
