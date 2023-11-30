9298b8a8a9e291ee3aa566538a6125425b4046e1
Корнилов А. А <KornilovAA@shuvoe.rg-rus.ru>
2023-11-30 14:20:24 +0300

    fast changes

---
+++ python/mdlp/lin/mail.py
@@ -0,0 +1,14 @@
+import smtplib
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+def send_mail_smtp(mail_server="mail.rg-rus.ru", mail_from, mail_to, mail_subject, mail_message):
+    msg = MIMEMultipart()
+    msg['Subject'] = mail_subject
+    msg['From'] = mail_from
+    msg['To'] = ', '.join(mail_to)
+    msg.attach(MIMEText(mail_message, 'plain'))
+    s = smtplib.SMTP(mail_server)
+    s.sendmail(mail_from, mail_to, msg.as_string())
+    s.quit()
