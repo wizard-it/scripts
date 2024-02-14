9be5171bf469810b56e16da5a0e4b2d4d5fa55b5
Корнилов А. А <KornilovAA@shuvoe.rg-rus.ru>
2024-02-14 13:22:45 +0300

    common patch

--- shell/mysql_dumpalldbs.sh
+++ shell/mysql_dumpalldbs.sh
@@ -3,31 +3,25 @@
 export PATH="/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin"
 
 # Destiny folder where backups are stored
-DEST=/opt/backup/mysql
+DEST=/usr/local/backup/mysql
 
-CURRDATE=$(date +"%F")
+#CURRDATE=$(date +"%F")
 
 # Hostname where MySQL is running
 HOSTNAME="localhost"
 # User name to make backup
-USER="backup"
+USER="root"
 # File where has the mysql user password
-PASS="$(cat /usr/local/etc/sqlpass)"
+#PASS="$(cat /usr/local/etc/sqlpass)"
 
-DATABASES=$(mysql -h $HOSTNAME -u $USER -p$PASS -e "SHOW DATABASES;" | tr -d "| " | grep -v Database)
+DATABASES=$(mysql -h $HOSTNAME -u $USER -e "SHOW DATABASES;" | tr -d "| " | grep -v Database)
 
 [ ! -d $DEST ] && mkdir -p $DEST
 
 for db in $DATABASES; do
   FILE="${DEST}/$db.sql.gz"
-  FILEDATE=
-
-  # Be sure to make one backup per day
-  [ -f $FILE ] && FILEDATE=$(date -r $FILE +"%F")
-  [ "$FILEDATE" == "$CURRDATE" ] && continue
-
   [ -f $FILE ] && mv "$FILE" "${FILE}.old"
-  mysqldump --single-transaction --routines --quick -h $HOSTNAME -u $USER -p$PASS -B $db | gzip > "$FILE"
+  mysqldump --single-transaction --set-gtid-purged=OFF --routines --quick -h $HOSTNAME -u $USER -B $db | gzip > "$FILE"
 #  chown bacula:disk "$FILE"
   rm -f "${FILE}.old"
 done
