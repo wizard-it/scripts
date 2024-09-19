a9a8dda5f96570530749dceb21678b4c9578bd65
Alexander Kornilov <spellbook33@gmail.com>
2024-09-19 14:31:47 +0300

    fixes

--- powershell/profiles/Microsoft.PowerShell_profile.ps1
+++ powershell/profiles/Microsoft.PowerShell_profile.ps1
@@ -1,16 +1,16 @@
 (Get-Host).UI.RawUI.CursorSize = 10
 (Get-Host).UI.RawUI.WindowTitle = $($(Get-Host).UI.RawUI.WindowTitle) + " :: " + $env:USERNAME
 
-function ConvertTo-Encoding ([string]$From, [string]$To){  
-    Begin{  
-        $encFrom = [System.Text.Encoding]::GetEncoding($from)  
-        $encTo = [System.Text.Encoding]::GetEncoding($to)  
-    }  
-    Process{  
-        $bytes = $encTo.GetBytes($_)  
-        $bytes = [System.Text.Encoding]::Convert($encFrom, $encTo, $bytes)  
-        $encTo.GetString($bytes)  
-    }  
+function ConvertTo-Encoding ([string]$From, [string]$To){
+    Begin{
+        $encFrom = [System.Text.Encoding]::GetEncoding($from)
+        $encTo = [System.Text.Encoding]::GetEncoding($to)
+    }
+    Process{
+        $bytes = $encTo.GetBytes($_)
+        $bytes = [System.Text.Encoding]::Convert($encFrom, $encTo, $bytes)
+        $encTo.GetString($bytes)
+    }
 }
 
 function prompt {
