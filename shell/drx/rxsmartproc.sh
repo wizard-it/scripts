04939cc80cd7aa1abeac60922836e5f2f497549c
Alexander Kornilov <spellbook33@gmail.com>
2025-02-06 18:15:11 +0300

    fast commit

---
+++ shell/drx/rxsmartproc.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+"/app/rxcmd/rxcmd" smart process-pkg -c "Default" $1 $2 $3 $4 $5
---
+++ shell/drx/rxsmartproc2.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+"/app/rxcmd/rxcmd" mailadapter get-letter -n 'Service User' -p pYLlkw3H2V -s 'https://test-rx.aodim.ru/integration/odata' $1 $2 $3 $4 $5
