f3548426716e365252a9065ef027da3d050efa3d
Alexander Kornilov <spellbook33@gmail.com>
2025-09-19 13:24:27 +0300

    fast commit

--- shell/rsync_push.sh
+++ shell/rsync_push.sh
@@ -3,13 +3,15 @@
 export PATH="/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin"
 
 # Target paths
-TARGETS="/RX/rxdata/logs/"
+TARGETS1="/RX/rxdata/logs/"
+TARGETS2=""
 
 # Destination
 HOST="10.15.20.121"
 
 # Destination resource
-SHARE="logs"
+SHARE1="logs"
+SHARE2=""
 
 # Login
 USER="rxuser"
@@ -19,6 +21,6 @@ SECRET="/etc/rsyncd.scrt"
 
 
 # BODY
-for f in $TARGETS; do 
-    rsync -rz --exclude '/status/' --exclude '/remote/' "$f" --password-file="$SECRET" "$USER"@"$HOST"::"$SHARE"
+for f in $TARGETS1; do 
+    rsync -rz --exclude '/status/' --exclude '/remote/' "$f" --password-file="$SECRET" "$USER"@"$HOST"::"$SHARE1"
 done
